c881345b7a5f9b4654847228c241603893728d9a
guilherme21alcantara <guilhermeafonsoalcantara@gmail.com>
2023-01-23 15:30:03 -0300

    commit adjusts

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({Key? key}) : super(key: key);
 
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  int perguntaSelecionada = 0;
+
+  int respondidas = 0;
   void responder() {
     setState(() {
-      perguntaSelecionada++;
+      if (perguntaSelecionada < 2) {
+        perguntaSelecionada++;
+      }
     });
     print('Pergunta Respondida!');
   }
@@ -17,27 +27,32 @@ class PerguntaAppState extends State<PerguntaApp> {
     final perguntas = [
       'Qual e a sua cor favorita?',
       'Qual e o seu animal favorito?',
+      'Qual é o maior planeta do sistema solar?'
     ];
 
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: const Text('Perguntas'),
+        title: const Center(
+            child: Text(
+          'Perguntas',
+          style: TextStyle(fontSize: 22),
+        )),
       ),
       body: Column(
         children: <Widget>[
           Text(perguntas[perguntaSelecionada]),
           ElevatedButton(
             child: Text('Resposta1'),
-            onPressed: responder,
+            onPressed: () => responder,
           ),
           ElevatedButton(
             child: Text('Resposta2'),
-            onPressed: responder,
+            onPressed: () => responder,
           ),
           ElevatedButton(
             child: Text('Resposta3'),
-            onPressed: responder,
+            onPressed: () => responder,
           ),
         ],
       ),
@@ -45,9 +60,9 @@ class PerguntaAppState extends State<PerguntaApp> {
   }
 }
 
-class PerguntaApp extends StatefulWidget {
-  @override
-  PerguntaAppState createState() {
-    return PerguntaAppState();
-  }
-}
+// class PerguntaApp extends StatefulWidget {
+//   @override
+//   PerguntaApp createState() {
+//     return PerguntaAppState();
+//   }
+// }
